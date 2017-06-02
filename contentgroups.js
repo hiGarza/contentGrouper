26f42eaacf9d2eb872c7da5bc4b46b4bb489314a
Humberto Garza <humberto@ooyala.com>
2017-06-02 15:41:10 -0500

    Add new CGs

--- contentgroups.js
+++ contentgroups.js
@@ -10,7 +10,6 @@ angular.module('contentgroups', [])
     };
 
     cg.breadCrumbSelect = function(cgBreadCrumb){
-      console.log(cgBreadCrumb["Name"]);
       var selectedCGIndex = cg.cgStack.findIndex(x => x["Id"]==cgBreadCrumb["Id"]);
       cg.cgStack = cg.cgStack.slice(0,selectedCGIndex+1);
       cg.fetchContentGroups(cg.cgStack[cg.cgStack.length - 1]["Id"]);
@@ -21,7 +20,42 @@ angular.module('contentgroups', [])
       cg.fetchContentGroups(cg.cgStack[cg.cgStack.length - 1]["Id"]);
     };
 
-    cg.cgStack = [{"Parent":"seed", "Id":"root", "Name":"root"}];
+    cg.addNewCG = function(){
+      cg.newCG = {
+        "Parent" : cg.cgStack[cg.cgStack.length - 1]["Id"],
+        "GrandParent" : cg.cgStack[cg.cgStack.length - 1]["Parent"]
+      };
+      cg.newCGTypeList = cg.templates.find(x => x["Id"] == cg.cgStack[cg.cgStack.length - 1]["Type"])["Childrens"];
+      cg.newCG["Type"] = cg.newCGTypeList[0];
+    };
+
+    cg.changenewCGType = function(newType){
+      cg.newCG["Type"] = newType;
+    };
+
+    cg.saveNewCG = function(){
+      $http.post("https://n0d9d0r79b.execute-api.us-west-1.amazonaws.com/Production/contentgroups", cg.newCG)
+      .then(function(response){
+        console.log(response);
+      }, function(error){console.log(error)});
+    };
+
+    cg.templates = [];
+    $http.get("https://n0d9d0r79b.execute-api.us-west-1.amazonaws.com/Production/contentgroups?Parent=Template")
+    .then(function(response){
+      cg.templates = response.data["Items"];
+    }, function(error){console.log(error)});
+
+    cg.cgStack = [{
+      "Childrens": [
+        "Serie",
+        "Video"
+      ],
+      "Id": "root",
+      "Name": "root",
+      "Parent": "seed",
+      "Type": "root"
+    }];
     cg.fetchContentGroups(cg.cgStack[cg.cgStack.length - 1]["Id"]);
 
   });
